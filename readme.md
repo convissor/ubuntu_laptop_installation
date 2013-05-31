f58e6cd44247264fabd9bb6aab778b4ea7b79745
Daniel Convissor <danielc@analysisandsolutions.com>
2013-05-30 22:37:06 -0400

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,142 @@
+Ubuntu Laptop Installation
+==========================
+
+Instructions and bash scripts for installing and configuring Ubuntu 12.04
+on a PC laptop.  Notable features:
+
+* Encrypts entire hard drive
+* Tracks all changes to `/etc` via git
+* Installs common utilities
+* Sets up administrative access via `ssh root@localhost`
+
+
+Prepare Installation Media
+==========================
+
+* [Obtain the `alternate` installer](http://releases.ubuntu.com/12.04/)
+* Put a USB stick in your existing computer.
+* As user with sudo powers:  System | Administration | Startup Disk Creator
+* Either add a partition to that thumb drive or get another drive and put
+the contents of this GitHub repository onto it:
+
+        usb_partition=transit
+        cp -R ubuntu_laptop_installation /media/$usb_partition
+
+        # To bring over other existing configurations for SSH, Firefox, etc.
+        cp -R ~/.ssh /media/$usb_partition
+        cp -R ~/.mozilla/firefox/<profile>/ /media/$usb_partition
+
+* UNMOUNT USB DRIVE
+
+
+Installation
+=============
+
+* Put USB drive in new computer
+* Hit the power button to turn the machine on
+
+The following three steps are described for a Lenovo laptops in 2012.  If
+you are using a different machine or BIOS versions, please improvise as
+needed.
+
+* When the BIOS splash screen appears, hit Enter, F12, select "USB HDD",
+hit Enter
+* When the small bar (USB logo?) appears, hit Enter
+* If things do not progress, hit Enter again
+
+Now you're in Ubuntu's installer.
+
+When asked for a user name, DO __NOT__ use your company assigned user name.
+A regular, company provided user name account will be created later.
+This first account is an admin user with sudo privileges; it should be a
+separate account from the one you use on a day-to-day basis for regular work.
+This prevents disruptive commands from being inadvertently executed.
+
+Once the "Partition disks" screen comes up, pick: `Guided - use entire disk
+and set up ecrypted LVM`
+
+
+Configuration
+=============
+
+Once Ubuntu is installed, it is time to configure the system for and
+install desirable utilities.
+
+Fire up a terminal session.  Here are two possible ways:
+
+1. ALT-F1, Enter, type in `terminal`, Enter
+1. CTRL-ALT-T
+
+Then issue the following commands:
+
+    chmod 700 .
+
+    sudo -i
+
+    mkdir -m 700 /root/.ssh
+    cd /root/.ssh
+    ssh-keygen -t rsa -C root@localhost -f id_rsa-root
+    cp id_rsa-root.pub authorized_keys
+
+    cat >> config <<EOSSH
+    ForwardAgent no
+    Host localhost
+    IdentityFile ~/.ssh/id_rsa-root
+    EOSSH
+
+    cp -R /media/<usb_partition>/ubuntu_laptop_installation .
+    cd install
+    chmod 700 *.sh
+    ./setup.sh
+
+The last step of the setup script updates the kernel.  If that's needed,
+it'll automatically reboot the computer.  If for some reason the reboot
+isn't required, that's cool.  Just log out from the administrator account.
+
+
+Wireless
+========
+
+* Log in to the laptop as the regular user
+* Click on the Network Connections icon in the tray, and click on
+"Edit Connections"
+* On the "Wireless" tab, click "Add"
+* Put the desired SSID
+* In the "Device MAC address" box, pick one
+* Uncheck "Connect automatically" and "Available to all users"
+* Go to the "Wireless Security" tab
+* Pick "WPA & WPA2 Personal" in the "Security" box
+* Enter the password
+* Click "Save"
+
+
+Regular User Customization
+==========================
+
+If you want to bring over a Firefox profile, log in as the LDAP user, then:
+
+    mkdir -p ~/.mozilla/firefox
+    cp -R /media/<usb_partition>/<profile> ~/.mozilla/firefox
+    firefox -ProfileManager &
+
+
+Future Reference
+================
+
+You can log in as root via `ssh root@localhost`
+
+To lock the screen, hit CTRL-ALT-L
+
+To check DNS settings, call `nm-tool`
+
+To change the password of the encrypted drive.  Watch bootup for which device
+is being decrypted (eg: `sda5_crypt`).  If the drive is something other than
+`sda5`, adjust the commands as needed.
+
+    cryptsetup luksAddKey /dev/sda5
+    cryptsetup luksDump /dev/sda5
+    cryptsetup luksKillSlot /dev/sda5 0
+
+Apparently, one can back up the drive encryption headers this way:
+
+    luksHeaderBackup /dev/sda5 --header-backup-file <file>
---
+++ setup.sh
@@ -0,0 +1,273 @@
+#! /bin/bash -e
+
+if [[ $1 == "-h" || $1 == "--help" || $1 == "help" ]] ; then
+    echo "Usage: setup.sh"
+    echo ""
+    echo "Configures a new Ubuntu 12.04 machine."
+    echo ""
+    echo "Author: Daniel Convissor <danielc@analysisandsolutions.com>"
+    echo "https://github.com/convissor/ubuntu_laptop_installation"
+    exit 1
+fi
+
+
+function ask_to_proceed() {
+    echo "> > > > >  $1 DONE"
+
+    # Uncomment the remaining lines if need to ask whether to proceed
+    # after each step.
+    #echo -n "Hit CTRL-C to stop or ENTER to continue... "
+    #read -e
+}
+
+function commit_if_needed() {
+    cd /etc
+    if [ -n "$(git status --porcelain)" ] ; then
+        git commit -qam "$1"
+    fi
+}
+
+
+# TRACK ALL CONFIGURATION CHANGES =========================
+
+step="git"
+apt-get -qq -y install git-core git-doc
+cd /etc
+git init
+chmod 770 .git
+
+git config --global user.name root
+git config --global user.email root@localhost
+
+echo "mtab" >> .gitignore
+echo "cups/subscriptions*" >> .gitignore
+git add --all
+commit_if_needed "$step"
+
+ask_to_proceed "$step"
+
+
+# SOFTWARE UPGRADE ========================================
+
+step="upgrade"
+apt-get -qq update && apt-get -qq -y upgrade
+cd /etc && git add --all && commit_if_needed "$step mods"
+ask_to_proceed "$step"
+
+
+# MAKE GRUB USABLE ========================================
+
+step="grub fixup"
+file=/etc/default/grub
+# Get grub menu to show by commenting out this option.
+sed -E "s/(GRUB_HIDDEN_TIMEOUT.*)/#\1/g" -i "$file"
+# Shorten timeout to 5 seconds.
+sed -E "s/(GRUB_TIMEOUT=.*)$/GRUB_TIMEOUT=5/g" -i "$file"
+
+# Get rid of "error: no video mode activated."
+# https://bugs.launchpad.net/ubuntu/+source/grub2/+bug/699802
+cp /usr/share/grub/*.pf2 /boot/grub
+
+update-grub
+
+cd /etc && git add --all && commit_if_needed "$step mods"
+ask_to_proceed "$step"
+
+
+# IPTABLES ================================================
+
+iptables_file=/etc/iptables/rules.v4
+ip6tables_file=/etc/iptables/rules.v6
+
+echo ""
+echo "Say NO both times to the pop up asking to save the IP tables data."
+echo -n "Press ENTER to continue..."
+read -e
+
+step="iptables-persistent"
+apt-get -qq -y install iptables-persistent
+cd /etc && git add --all && commit_if_needed "$step"
+
+dir=`dirname "$iptables_file"`
+if [ ! -d "$dir" ] ; then
+    mkdir -p "$dir"
+fi
+
+# -------------------------------------
+cat > "$iptables_file" <<EOIPT
+*filter
+# Drop inbound packets unless specifically allowed by subsequent rules.
+:INPUT DROP [0:0]
+:FORWARD DROP [0:0]
+
+# Outbound packets are fine.
+:OUTPUT ACCEPT
+
+# Allow HTTP,HTTPS.
+#-A INPUT -p tcp -m multiport --dports 80,443 -j ACCEPT
+
+# Allow responses our outgoing transmissions.
+-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# Allow everything on loopback adapater.
+-A INPUT -i lo -j ACCEPT
+
+COMMIT
+EOIPT
+# -------------------------------------
+
+cp "$iptables_file" "$ip6tables_file"
+/etc/init.d/iptables-persistent restart
+cd /etc && git add --all && commit_if_needed "$step mods"
+ask_to_proceed "$step"
+
+
+# FAIL2BAN ================================================
+
+step="fail2ban"
+apt-get -qq -y install fail2ban
+cd /etc && git add --all && commit_if_needed "$step"
+
+file=/etc/fail2ban/jail.conf
+# Increase lockout length from 10 minutes to 1 day.
+sed -E "s/^bantime\s+=.*/bantime = 86400/g" -i "$file"
+
+ask_to_proceed "$step"
+
+
+# LOGIN UI CLEANUP ========================================
+
+step="disable guest sessions"
+echo "greeter-hide-users=true" >> /etc/lightdm/lightdm.conf
+echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
+cd /etc && git add --all && commit_if_needed "$step"
+ask_to_proceed "$step"
+
+
+# SSHD ====================================================
+
+step="sshd"
+apt-get install -qq -y openssh-server openssh-blacklist openssh-blacklist-extra
+cd /etc && git add --all && commit_if_needed "$step"
+
+file=/etc/ssh/sshd_config
+sed -E "s/^PermitRootLogin\s+(.*)/PermitRootLogin without-password/g" -i "$file"
+commit_if_needed "$step mods"
+
+service ssh reload
+ask_to_proceed "$step"
+
+
+# MISC ====================================================
+
+step="misc tools"
+
+echo ""
+echo "During the next step, you'll be asked to set up Postfix."
+echo "Pick 'Local only' from the list."
+echo -n "Press ENTER to continue..."
+read -e
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub \
+    | apt-key add -
+
+file=/etc/apt/sources.list
+sed -E "s/^# (deb.* partner)$/\1/g" -i "$file"
+echo "" >> "$file"
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> "$file"
+cd /etc && commit_if_needed "Allow 'partner' packages."
+apt-get update
+
+apt-get -qq -y install \
+    git-svn git-cvs gitk subversion subversion-tools cvs mercurial \
+    konversation skype mutt links lynx google-chrome-stable curl \
+    flashplugin-installer qt4-qtconfig \
+    vim exuberant-ctags \
+    epdfview pdfedit okular okular-extra-backends poppler-utils \
+    antiword tofrodos ack-grep \
+    sqlite3 sqlite3-doc \
+    ntp myunity gparted lm-sensors htop screen \
+    gimp gimp-help-en
+
+ln -s /usr/bin/ack-grep /usr/bin/ack
+restart network-manager
+
+# Have unattended-upgrades run automatically.
+file=/etc/apt/apt.conf.d/10periodic
+grep -q "APT::Periodic::Unattended-Upgrade" "$file"
+if [ $? -eq 0 ] ; then
+    # Something is in there. Make sure it's enabled.
+    sed -E 's@^/*(\s*APT::Periodic::Unattended-Upgrade\s+)"[0-9]+"@\1"1"@g' -i "$file"
+else
+    # Nothing is in there. Add it.
+    echo 'APT::Periodic::Unattended-Upgrade "1";' >> "$file"
+fi
+
+cd /etc && git add --all && commit_if_needed "$step"
+ask_to_proceed "$step"
+
+
+# REGULAR USER ============================================
+
+step="regular user"
+
+loop_again=1
+while [ $loop_again -eq 1 ] ; do
+    echo -n "Creating regular user account.  Enter the LDAP user name for it:"
+    read user
+
+    if [ -z "$user" ] ; then
+        echo "ERROR: Really, you need to do this..."
+        loop_again=1
+    else
+        loop_again=0
+    fi
+done
+
+adduser "$user"
+
+if [ -d /root/.ssh ] ; then
+    cp -R /root/.ssh "/home/$user"
+    chown "$user":"$user" "/home/$user"
+    chmod 700 "/home/$user"
+    chmod 600 "/home/$user"/*
+fi
+
+cd /etc && git add --all && commit_if_needed "$step"
+ask_to_proceed "$step"
+
+
+# VIM SETTINGS ========================================
+
+echo -n "Should we give each user Dan's vim settings? [N|y]: "
+read -e
+if [[ "$REPLY" == y || "$REPLY" == Y ]] ; then
+    cd
+    git clone git://github.com/convissor/vim-settings.git
+    cd vim-settings
+    ./setup.sh
+
+    dirs=`ls /home`
+
+    for dir in $dirs ; do
+        cd "/home/$dir"
+        cp -R /root/vim-settings .
+        cd vim-settings
+        ./setup.sh
+        cd ..
+        chown -R "$dir":"$dir" .vim .vimrc vim-settings
+    done
+fi
+
+
+# KERNEL UPGRADE ========================================
+
+step="kernel upgrade"
+apt-get -qq update && apt-get -qq -y dist-upgrade
+cd /etc && git add --all && commit_if_needed "$step mods"
+if [ -a /var/run/reboot-required ] ; then
+    echo "REBOOT IS REQURED"
+    echo -n "Press ENTER to continue..."
+    read -e
+    shutdown -r now
+fi
