1f32cd9021ba252caa1eb87cb99152effb768b6f
Daniel Convissor <danielc@analysisandsolutions.com>
2014-03-19 16:54:55 -0400

    Install autoconf.

--- setup.sh
+++ setup.sh
@@ -288,6 +288,7 @@ apt-get -qq update
 
 apt-get -qq -y install \
     git-svn git-cvs gitk subversion subversion-tools cvs mercurial \
+    autoconf autoconf-doc \
     konversation skype mutt links lynx google-chrome-stable curl \
     gnome-mplayer flashplugin-installer qt4-qtconfig \
     vim \
