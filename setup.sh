8fb9872a7f296e0df1be705b7efb4066d8864e13
Daniel Convissor <danielc@analysisandsolutions.com>
2015-07-11 22:52:43 -0400

    More packages.

--- setup.sh
+++ setup.sh
@@ -388,19 +388,23 @@ apt-get -qq update
 
 apt-get -qq -y install \
     git-svn git-cvs gitk subversion subversion-tools cvs mercurial bzr \
-    autoconf autoconf-doc \
-    konversation skype mutt links lynx google-chrome-stable curl \
-    gnome-mplayer flashplugin-installer qt4-qtconfig \
-    okular okular-extra-backends poppler-utils \
-    antiword tofrodos ack-grep gawk \
-    sqlite3 sqlite3-doc \
+    autoconf autoconf-doc autoconf2.13 automake1.4 bison build-essential kcachegrind \
+    konversation liferea skype mutt mb2md maildir-utils \
+    links lynx google-chrome-stable curl icedtea-plugin w3m \
+    mencoder mplayer mplayer-doc flashplugin-installer qt4-qtconfig \
+    okular okular-extra-backends pdfposter pdftk poppler-utils \
+    antiword tofrodos ack-grep gawk html2text uni2ascii tidy xz-lzma \
+    dictd dict-gcide dict-moby-thesaurus \
+    apache2-doc apache2-mpm-prefork apache2-prefork-dev lighttpd memcached \
+    mysql-client mysql-server sqlite sqlite-doc sqlite3 sqlite3-doc \
     virtualbox \
-    ntp traceroute gparted lm-sensors htop screen \
+    ntp openvpn traceroute wireshark \
+    gparted lm-sensors htop screen mcrypt \
     unity-tweak-tool gnome-panel gnome-tweak-tool \
-    gimp gimp-help-en
+    gimp gimp-help-en imagemagick
 
 # Packages not available in vivid (as of yet):
-# epdfview pdfedit myunity gnome-session-fallback
+# myunity gnome-session-fallback
 
 # Multimedia codecs and DVD playback.
 apt-get -qq -y install ubuntu-restricted-extras
