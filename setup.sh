b534ea88a8ab9798adccd7dc8d100f781ef597a4
Daniel Convissor <danielc@analysisandsolutions.com>
2015-11-18 10:17:42 -0500

    Update virtualbox pkg source and name.

--- setup.sh
+++ setup.sh
@@ -452,7 +452,7 @@ sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub \
 cd /etc && git add --all && commit_if_needed "Add Google Chrome repository."
 
 file=/etc/apt/sources.list.d/virtualbox.list
-echo "deb http://download.virtualbox.org/virtualbox/debian vivid non-free" > "$file"
+echo "deb http://download.virtualbox.org/virtualbox/debian vivid contrib" > "$file"
 wget -q -O - http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc \
     | apt-key add -
 cd /etc && git add --all && commit_if_needed "Add Virtualbox repository."
@@ -539,7 +539,8 @@ apt-get -qq -y install \
     sqlite3 \
     sqlite3-doc \
     \
-    virtualbox \
+    virtualbox-5.0 \
+    dkms \
     \
     ntp \
     openvpn \
